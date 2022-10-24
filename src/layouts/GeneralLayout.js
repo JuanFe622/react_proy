56ed185dba10ba8e71ba762b8cb9ffff156cfcdc
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2022-10-24 18:28:58 -0500

    Punto 3

--- src/layouts/GeneralLayout.js
+++ src/layouts/GeneralLayout.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { Layout } from "antd";
+import React, { useState } from "react";
+import { Button, Layout } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
+import MenuSider from "../components/MenuComponents/MenuSider";
+import MenuTop from "../components/MenuComponents/MenuTop";
+import "./GeneralLayout.scss";
 
-export default function GeneralLayout(props){
+export default function GeneralLayout(props) {
+    const [menuCollapsed, setMenuCollapsed] = useState(false);
+    const { Header, Content, Footer} = Layout;
     const { children } = props;
     return (
         <Layout>
-            <h2>Menu</h2>
-            <div>Contenido</div>
-            <h5>Footer</h5>
-            {children}
+            <MenuSider menuCollapsed={menuCollapsed}/>
+            <Layout style={{ marginLeft: menuCollapsed ? "80px" : "200px" }} >
+                <Header>
+                    <MenuTop
+                        menuCollapsed={menuCollapsed}
+                        setMenuCollapsed={setMenuCollapsed}
+                    />
+                </Header>
+                <Content className="layout-admin__content">{children}</Content>
+                <Footer className="layout-admin__footer">
+                    <Button type="link" onClick={() => console.log("Github")}>
+                        <GithubOutlined style={{ fontSize: "17px" }}/> UAM 2022
+                    </Button>
+                </Footer>
+            </Layout>
         </Layout>
     );
-};
+}
